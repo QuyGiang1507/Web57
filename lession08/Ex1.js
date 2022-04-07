ffdd5caeee2372bb769e0e8a980f088fae8cb5f8
QuyGiang1507 <phungminhquygiang@gmail.com>
2022-04-07 20:45:33 +0700

    homework08

--- lession08/Ex1.js
+++ lession08/Ex1.js
@@ -5,7 +5,7 @@ app.use(express.json())
 
 app.get('/api/test', (req, res, next) => {
     const byPast = req.query.byPast;
-    if(byPast === 1) {
+    if(byPast === "1") {
         next();
     } 
     res.send({ success : 0, message: 'Invalid params' })
